11e9efeac5cfab65ca4043df7f1978221ac94257
Nax Oduor <naxoduor7@gmil.com>
2026-08-30 14:35:44 +0300

    correct flow

--- src/main/java/com/bio/sequencing/scheduling/MyScheduledTask.java
+++ src/main/java/com/bio/sequencing/scheduling/MyScheduledTask.java
@@ -1,18 +1,64 @@
 package com.bio.sequencing.scheduling;
 
+import com.bio.sequencing.port.Port;
+import com.bio.sequencing.workers.Actor;
+import com.bio.sequencing.workers.AlignerWorker;
+import com.bio.sequencing.workers.FastaReaderWorker;
+import com.bio.sequencing.workers.ParserWorker;
+import com.bio.sequencing.workflow.WorkflowExecutor;
+import com.bio.sequencing.workflow.WorkflowGraph;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 @Component
 public class MyScheduledTask {
 
-    @Scheduled(fixedRate = 1000)
-    public void executeEverySecond() {
-        System.out.println("Running...");
-    }
-
     @Scheduled(fixedRate = 60_000)
     public void executeEveryMinute() {
         // business logic
+        Queue<Object> channel1 = new LinkedList<>();
+        Queue<Object> channel2 = new LinkedList<>();
+
+        Port readerOutput = new Port(channel1);
+        Port parserInput = new Port(channel1);
+
+        Port parserOutput = new Port(channel2);
+        Port alignerInput = new Port(channel2);
+
+        FastaReaderWorker reader =
+                new FastaReaderWorker(readerOutput);
+
+        ParserWorker parser =
+                new ParserWorker(
+                        parserInput,
+                        parserOutput);
+
+        AlignerWorker aligner =
+                new AlignerWorker(
+                        alignerInput,
+                        new Port(new LinkedList<>()));
+
+        Actor readerActor =
+                new Actor("reader", reader);
+
+        Actor parserActor =
+                new Actor("parser", parser);
+
+        Actor alignerActor =
+                new Actor("aligner", aligner);
+
+        WorkflowGraph graph = new WorkflowGraph();
+
+        graph.addActor(readerActor);
+        graph.addActor(parserActor);
+        graph.addActor(alignerActor);
+
+        WorkflowExecutor executor =
+                new WorkflowExecutor(graph);
+
+        executor.execute();
     }
 }
